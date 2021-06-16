641a7a719f3d10f6677b25efe63c527e57123c68
Sam Li <55217028+wsli1998@users.noreply.github.com>
2021-06-16 12:18:46 +0800

    sanity setup

--- src/App.js
+++ src/App.js
@@ -2,72 +2,34 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import '../node_modules/react-vis/dist/style.css';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
-import { VictoryBar ,VictoryPie} from 'victory';
 
 
-const data = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
+
 function App() {
-  return (
-    < div className = "App" >
-      <h1> Hello Sam! </h1>
-      
-<VictoryPie
-width={600}
-height={300}>
-  data={[
-    { x: "Cats", y: 35 },
-    { x: "Dogs", y: 40 },
-    { x: "Birds", y: 55 }
-  ]}
-/>
-
-      <VictoryBar
-        data={data}
-        // data accessor for x values
-        x="quarter"
-        // data accessor for y values
-        y="earnings"
-      />
-
-      <XYPlot
-        width={600}
-        height={300}>
-        <HorizontalGridLines />
-        <LineSeries
-          data={[
-            {x: 1, y: 10},
-            {x: 2, y: 5},
-            {x: 3, y: 15},
-            {x: 4, y: 15},
-            {x: 5, y: 15}
-
-          ]}/>
-        <XAxis />
-        <YAxis />
-      </XYPlot>
+  return ( <
+    div className = "App" >
+    <
+    h1 > Hello Sam! < /h1>
 
 
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+
+    {
+      /* <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header> */
+    } <
+    /div>
   );
 }
 
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from 'sanity/client'
+
+export default sanityClient{
+    projectID:"l8gdsoha",
+    dataset:"production"
+}
